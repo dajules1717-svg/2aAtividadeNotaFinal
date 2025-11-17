8c2bad158b766f17ab448cf6350d7b811b24f452
Julia <dajules1717@gmail.com>
2025-11-17 12:17:13 -0300

    Delete sistema_de_tarefas directory

--- sistema_de_tarefas/delete_tarefa.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'database.php';
-if(isset($_GET['id'])){
-    $id = $_GET['id'];
-    $stmt = $conn->prepare("DELETE FROM tarefas WHERE id = ?");
-    $stmt->execute([$id]);
-}
-header("Location: index.php");
-?>
--- sistema_de_tarefas/update_tarefa.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'database.php';
-if(isset($_GET['id'])){
-    $id = $_GET['id'];
-    $stmt = $conn->prepare("UPDATE tarefas SET concluida = 1 WHERE id = ?");
-    $stmt->execute([$id]);
-}
-header("Location: index.php");
-?>
